d9eff61350e37be68c0a67eaa6da9a9f6779a00c
QDSG <piftao1127@163.com>
2020-05-06 16:39:41 +0800

    在循环中创建视图-01

--- Demo01/WeSplit/WeSplit/ContentView.swift
+++ Demo01/WeSplit/WeSplit/ContentView.swift
@@ -9,13 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
     
     var body: some View {
         Form {
-            TextField("Enter your name", text: $name)
-            Text("Hello World")
-            Text("Your name is \(name)")
+//            ForEach(0 ..< 100) { number in
+//                Text("Row \(number)")
+//            }
+            ForEach(0 ..< 100) {
+                Text("Row \($0)")
+            }
         }
     }
 }
