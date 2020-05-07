ae046ac78bf0aa2a5d6af4d08a984799a8ede8e1
QDSG <piftao1127@163.com>
2020-05-07 14:39:47 +0800

    将视图作为属性

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -13,11 +13,38 @@ struct ContentView: View {
     /// 1. 条件修饰符
     @State private var useRedText = false
     
+    /// 3. 将视图作为属性
+    var motto1: some View { return Text("Draco dormiens") }
+    let motto2 = Text("nunquam titillandus")
+    
     var body: some View {
-        Button("Hello World") {
-            self.useRedText.toggle()
+        VStack(spacing: 30) {
+            Button("Hello World") {
+                self.useRedText.toggle()
+            }
+            .foregroundColor(useRedText ? .red : .blue)
+            
+            /// 2. 环境修饰符
+            VStack {
+                /// 子视图的修饰符优先
+                Text("Gryffindor")
+                    .font(.headline)
+                // 常规修饰符, 应用于子视图的任何模糊都会添加到VStack模糊之上，而不是替换它
+                    .blur(radius: 0)
+                Text("Hufflepuff")
+                Text("Ravenclaw")
+                Text("Slytherin")
+            }
+            .font(.title)
+            .blur(radius: 5)
+            
+            VStack {
+                motto1
+                    .foregroundColor(.red)
+                motto2
+                    .foregroundColor(.blue)
+            }
         }
-        .foregroundColor(useRedText ? .red : .blue)
     }
 }
 
