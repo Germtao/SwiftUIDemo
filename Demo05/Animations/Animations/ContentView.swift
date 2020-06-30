fb54d21ed0542d46daaad398c48e73f62e0739d9
QDSG <piftao1127@163.com>
2020-06-30 11:25:06 +0800

    创建显式动画

--- Demo05/Animations/Animations/ContentView.swift
+++ Demo05/Animations/Animations/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var animAmount: CGFloat = 1
+    @State private var animAmount1 = 0.0
     
     var body: some View {
         print(animAmount)
@@ -35,6 +36,22 @@ struct ContentView: View {
             .foregroundColor(.white)
             .clipShape(Circle())
             .scaleEffect(animAmount)
+            
+            Spacer()
+            
+            Button("显示动画") {
+//                withAnimation {
+//                    self.animAmount1 += 360
+//                }
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                    self.animAmount1 += 360
+                }
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animAmount1), axis: (x: 0, y: 1, z: 0)) // 沿Y轴旋转
             
             Spacer()
         }
