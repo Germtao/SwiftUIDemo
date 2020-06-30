e0d508d22227ca948a4929c3a564d2ab1bcc9bfa
QDSG <piftao1127@163.com>
2020-06-30 10:56:38 +0800

    自定义动画

--- Demo05/Animations/Animations/ContentView.swift
+++ Demo05/Animations/Animations/ContentView.swift
@@ -14,27 +14,25 @@ struct ContentView: View {
     
     var body: some View {
         Button("隐式动画") {
-            self.animAmount += 1
+//            self.animAmount += 1
         }
         .padding(50)
         .background(Color.red)
         .foregroundColor(Color.white)
         .clipShape(Circle())
-        .scaleEffect(animAmount) // 放大
-        .animation(
-            Animation.easeInOut(duration: 1.0)
-//                .delay(1) // 延时
-            // 重复一定次数，甚至可以通过将autoreverses设置为true来使其前后反弹
-//            .repeatCount(3, autoreverses: true)
-            .repeatForever(autoreverses: true)
+        .overlay( // 按钮周围形成一种脉动的圆圈
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animAmount)
+                .opacity(Double(2 - animAmount))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
         )
-        
-        // 弹簧动画
-//        .animation(
-//            // stiffness: 初始刚度 即初速度
-//            // damping: 阻尼 即摩擦力
-//            .interpolatingSpring(stiffness: 20, damping: 0.5)
-//        )
+        .onAppear {
+            self.animAmount = 2
+        }
     }
 }
 
