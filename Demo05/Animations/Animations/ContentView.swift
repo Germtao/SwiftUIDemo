64895cd56f65329fae8743644dbcdb7a65a54285
QDSG <piftao1127@163.com>
2020-06-30 09:50:49 +0800

    init

---
+++ Demo05/Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by QDSG on 2020/6/30.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
