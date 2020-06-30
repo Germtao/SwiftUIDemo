05b9b44e894bcd14ede2b071a5349a06dff21c49
QDSG <piftao1127@163.com>
2020-06-30 17:53:27 +0800

    在SwiftUI中使用可标识的项目

--- Demo06/iExpense/iExpense/ExpenseItem.swift
+++ Demo06/iExpense/iExpense/ExpenseItem.swift
@@ -8,7 +8,9 @@
 
 import Foundation
 
-struct ExpenseItem {
+// Identifiable: Swift内置的协议之一，表示“可以唯一地标识此类型”
+// 它只有一个要求，那就是必须有一个名为id的属性，其中包含唯一的标识符
+struct ExpenseItem: Identifiable {
     let id = UUID() // 唯一的标志
     let name: String
     let type: String
