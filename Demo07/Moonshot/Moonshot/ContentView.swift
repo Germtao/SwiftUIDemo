6224c4244706b69c647657dff15042736a6bb4d4
QDSG <piftao1127@163.com>
2020-07-01 14:59:41 +0800

    init

---
+++ Demo07/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by QDSG on 2020/7/1.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
