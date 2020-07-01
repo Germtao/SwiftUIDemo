8b70952d67b2b72e16290fb03c4afa06c7972ca4
QDSG <piftao1127@163.com>
2020-07-01 14:15:55 +0800

    使用NavigationLink将新视图压入堆栈

---
+++ Demo07/NavigationLinkDemo/NavigationLinkDemo/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  NavigationLinkDemo
+//
+//  Created by QDSG on 2020/7/1.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
+                }
+            }
+            .navigationBarTitle("SwiftUI")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
