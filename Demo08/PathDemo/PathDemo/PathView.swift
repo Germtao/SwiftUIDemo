95c400ed1f991340deb7f1ac7085ae1861b67b04
QDSG <piftao1127@163.com>
2020-07-02 14:45:42 +0800

    路径、形状

---
+++ Demo08/PathDemo/PathDemo/PathView.swift
@@ -0,0 +1,59 @@
+//
+//  PathView.swift
+//  PathDemo
+//
+//  Created by QDSG on 2020/7/2.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct PathView: View {
+    var body: some View {
+        NavigationView {
+            GeometryReader { geo in
+                ScrollView(.vertical) {
+                    VStack(spacing: 10) {
+    //                    Path { path in
+    //                        path.move(to: CGPoint(x: 200, y: 100))
+    //                        path.addLine(to: CGPoint(x: 100, y: 300))
+    //                        path.addLine(to: CGPoint(x: 300, y: 300))
+    //                        path.addLine(to: CGPoint(x: 200, y: 100))
+    //                        path.addLine(to: CGPoint(x: 100, y: 300))
+    //                    }
+    //            //        .fill(Color.blue)
+    //                    .stroke(
+    //                        Color.blue,
+    //                        style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+    //                    )
+                                
+                        Triangle()
+            //                .fill(Color.red)
+                            .stroke(
+                                Color.red,
+                                style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+                            )
+                            .frame(width: 200, height: 200)
+                        
+                        Spacer()
+                        
+                        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+                            .strokeBorder(Color.blue, lineWidth: 40)
+    //                        .stroke(Color.blue, lineWidth: 40)
+                            .frame(width: 200, height: 200)
+                        
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+        }
+        .navigationBarTitle("Path、Shade等", displayMode: .inline)
+    }
+}
+
+struct PathView_Previews: PreviewProvider {
+    static var previews: some View {
+        PathView()
+    }
+}
