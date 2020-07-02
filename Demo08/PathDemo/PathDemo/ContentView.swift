b136419dbbb1a406c830432e66f94b85e9e0293b
QDSG <piftao1127@163.com>
2020-07-02 11:01:51 +0800

    路径 Path vs 形状 Shape

--- Demo08/PathDemo/PathDemo/ContentView.swift
+++ Demo08/PathDemo/PathDemo/ContentView.swift
@@ -8,20 +8,46 @@
 
 import SwiftUI
 
+// Path: 路径被设计为做一件特定的事情
+// Shape: 形状具有绘制空间的灵活性，并且还可以接受参数以使我们进一步自定义它们
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
+        GeometryReader { geo in
+            ScrollView(.vertical) {
+                VStack(spacing: 10) {
+//                    Path { path in
+//                        path.move(to: CGPoint(x: 200, y: 100))
+//                        path.addLine(to: CGPoint(x: 100, y: 300))
+//                        path.addLine(to: CGPoint(x: 300, y: 300))
+//                        path.addLine(to: CGPoint(x: 200, y: 100))
+//                        path.addLine(to: CGPoint(x: 100, y: 300))
+//                    }
+//            //        .fill(Color.blue)
+//                    .stroke(
+//                        Color.blue,
+//                        style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+//                    )
+                            
+                    Triangle()
+        //                .fill(Color.red)
+                        .stroke(
+                            Color.red,
+                            style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+                        )
+                        .frame(width: 200, height: 200)
+                    
+                    Spacer()
+                    
+                    Arc(startAngle: .degrees(0), endAngle: .degrees(100), clockwise: true)
+                        .stroke(Color.blue, lineWidth: 10)
+                        .frame(width: 200, height: 200)
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+            }
         }
-//        .fill(Color.blue)
-        .stroke(
-            Color.blue,
-            style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
-        )
     }
 }
 
---
+++ Demo08/PathDemo/PathDemo/Triangle.swift
@@ -0,0 +1,45 @@
+//
+//  Triangle.swift
+//  PathDemo
+//
+//  Created by QDSG on 2020/7/2.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+/// 形状 Shape
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(
+            center: CGPoint(x: rect.midX, y: rect.midY),
+            radius: rect.width / 2,
+            startAngle: modifiedStart,
+            endAngle: modifiedEnd,
+            clockwise: clockwise
+        )
+        return path
+    }
+}
