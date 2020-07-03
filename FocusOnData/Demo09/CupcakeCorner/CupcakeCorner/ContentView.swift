df5b87bc35fe12804ca02db009904e95f48af066
tTao <>
2020-07-03 22:57:08 +0800

    获取基本订单详细信息

--- FocusOnData/Demo09/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ FocusOnData/Demo09/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var order = Order()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("选择蛋糕类型", selection: $order.type) {
+                        ForEach(0..<Order.types.count, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("数量: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("特殊要求？")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("加糖")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("家巧克力")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("订单详情")
+                    }
+                }
+            }
+            .navigationBarTitle("纸杯蛋糕")
+        }
     }
 }
 
---
+++ FocusOnData/Demo09/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,38 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by TT on 2020/7/3.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    /// 所有可能选项的静态数组
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    /// 蛋糕的类型
+    @Published var type = 0
+    
+    /// 用户想要订购多少蛋糕
+    @Published var quantity = 3
+    
+    /// 用户是否要提出特殊请求
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    /// 用户是否要在蛋糕上加糖霜
+    @Published var extraFrosting = false
+    
+    /// 用户是否要在蛋糕上撒些巧克力
+    @Published var addSprinkles = false
+    
+    
+}
