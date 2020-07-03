4cca70f38b491d3403b663c012dc53795690eac9
QDSG <piftao1127@163.com>
2020-07-03 14:53:57 +0800

    init

---
+++ Demo08/Spirograph/Spirograph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spirograph
+//
+//  Created by QDSG on 2020/7/3.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
