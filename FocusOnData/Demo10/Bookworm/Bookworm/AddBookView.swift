f24bddc0491be725d0fe2838b2015d7629f20956
QDSG <piftao1127@163.com>
2020-07-08 18:02:41 +0800

    todo

---
+++ FocusOnData/Demo10/Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,23 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by QDSG on 2020/7/8.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Enviro
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
