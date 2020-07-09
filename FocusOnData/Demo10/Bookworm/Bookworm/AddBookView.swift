bc4b5f30b53c6945ed5c682287d7acdd1a4ad985
QDSG <piftao1127@163.com>
2020-07-09 11:43:23 +0800

    使用 Core Data 创建书籍

--- FocusOnData/Demo10/Bookworm/Bookworm/AddBookView.swift
+++ FocusOnData/Demo10/Bookworm/Bookworm/AddBookView.swift
@@ -9,10 +9,60 @@
 import SwiftUI
 
 struct AddBookView: View {
-    @Enviro
+    @Environment(\.managedObjectContext) var moc
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    /// 流派选项
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("书名", text: $title)
+                    TextField("作者", text: $author)
+                    
+                    Picker("流派", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("评分", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text("\($0)")
+                        }
+                    }
+                    
+                    TextField("评论", text: $review)
+                }
+                
+                Section {
+                    Button("保存") {
+                        self.addBook()
+                    }
+                }
+            }
+            .navigationBarTitle("添加书本")
+        }
+    }
+    
+    func addBook() {
+        let newBook = Book(context: moc)
+        newBook.title = title
+        newBook.author = author
+        newBook.genre = genre
+        newBook.rating = Int16(rating)
+        newBook.review = review
+        
+        try? moc.save()
     }
 }
 
