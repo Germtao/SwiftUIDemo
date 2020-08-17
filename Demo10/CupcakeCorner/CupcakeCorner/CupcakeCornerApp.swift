45e962b6cd58d5fc184955a8028683d4ca5095e8
QDSG <piftao1127@163.com>
2020-08-17 15:22:43 +0800

    init

---
+++ Demo10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by QDSG on 2020/8/17.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
