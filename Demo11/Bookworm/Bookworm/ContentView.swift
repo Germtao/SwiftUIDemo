8e5ade8754d9724cf6d58ea528cb757f271b8a6c
QDSG <piftao1127@163.com>
2020-08-20 17:56:56 +0800

    init

---
+++ Demo11/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by QDSG on 2020/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
