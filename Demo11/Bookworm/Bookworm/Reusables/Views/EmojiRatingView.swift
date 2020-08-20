3f29e801cf797cad54322b721e956b618b5b7940
QDSG <piftao1127@163.com>
2020-08-20 17:57:26 +0800

    评分视图

---
+++ Demo11/Bookworm/Bookworm/Reusables/Views/EmojiRatingView.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by QDSG on 2020/8/20.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+}
+
+extension EmojiRatingView {
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("1")
+        case 2:
+            return Text("2")
+        case 3:
+            return Text("3")
+        case 4:
+            return Text("4")
+        default:
+            return Text("5")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
